9f1b0e135bfbcb406e375728550e18c0af4ec720
micheledimassimo <mcldms@outlook.it>
2024-09-01 21:05:34 +0200

    v1.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
     createApp({
         data() {
             return {
-                message: 'Template Vue'
+                todos: [
+                    {
+                        text: 'spesa',
+                        done: false
+                    },
+                    {
+                        text: 'esercizio js',
+                        done: true
+                    },
+                    {
+                        text: 'pulizia casa',
+                        done: false
+                    }
+                ],
+                verified: '',
+                newTask: {
+                    text: '',
+                    done: ''
+                }
+            }
+        },
+        methods: {
+            addTask() {
+                if (this.newTask.text != '') {
+                    this.todos.push(this.newTask.text);
+
+                    this.newTask.text = '';
+                }
+                
+            },
+            removeTask(i) {
+                this.todos.splice(i, 1);
             }
         }
     }).mount('#app');
